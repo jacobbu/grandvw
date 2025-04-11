e4a3dfcc51f91df13ef8edb9d5361c8640c8e344
jacobbu <r.jacob.buckner@gmail.com>
2025-04-11 14:03:07 -0600

    sms consent language

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,13 +1,31 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django import forms
+from django.contrib.auth import get_user_model
 
-from .models import User
+User = get_user_model()
 
 class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
 
+    def __init__(self, *args, **kwargs):
+        super(SignupForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({
+                'class': 'w-full p-3 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-gray-700 focus:border-transparent',
+                'placeholder': field.label
+            })
+            field.help_text = None
+            field.label = ""        
+
 class LoginForm(AuthenticationForm):
-    class Meta:
-        model = User
-        fields = ('username', 'password')
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({
+                'class': 'w-full p-3 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-gray-700 focus:border-transparent',
+                'placeholder': field.label
+            })
+            field.help_text = None  
+            field.label = ""        
